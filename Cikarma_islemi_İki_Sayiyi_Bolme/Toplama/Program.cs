50d9fb236cc857afc5c8502a036baae35bb3a7b1
Serkan <serkankabadayiogullari8233@gmail.com>
2023-09-24 18:44:17 +0300

    CİKARMA İSLEMİ İLE İKİ SAYİYİ BOLME İSLEMİ
    ÇIKARMA İŞLEMİNİ KULLANARAK BÖLME SONUCU BULAN KODU YAZDIM

---
+++ Cikarma_islemi_İki_Sayiyi_Bolme/Toplama/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Channels;
+
+namespace Toplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bölünecek sayıyı giriniz:");
+            double sayi1=double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Bölen sayıyı giriniz:");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            double bolum = 0;
+            do
+            {
+                sayi1 -= sayi2;
+                bolum++;
+                if (sayi1 < sayi2)
+                    break;
+
+            } while (sayi1>0);
+            Console.WriteLine(" Do ile sonuç: " + bolum);
+            
+            while (sayi1>0)
+            {
+                sayi1 -= sayi2;
+                bolum++;
+                if (sayi1 < sayi2)
+                    break;
+
+            }
+            Console.WriteLine("While ile sonuç: " + bolum);
+
+            for (int i = 0; i < sayi1; i++)
+            {
+                sayi1 -= sayi2;
+                bolum--;
+                if (sayi1 < sayi2)
+                    break;
+            }
+            Console.WriteLine("For ile sonuç: " + bolum);
+            Console.Read();
+
+            Console.Read();
+
+        }
+    }
+}
